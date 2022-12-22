f2c415d10188cd0201429f88f972728899020eb8
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2022-12-22 14:31:23 -0500

    added state management on inputs

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -15,27 +15,27 @@ export default function Login() {
       <div className="left">
           
       </div>
-      <form action="" className="log-in" autocomplete="off"> 
-        <h4>Collège Jean de la Mennais</h4>
+      <div className="form log-in"> 
+        <h1>Collège Jean de la Mennais</h1>
         <div className="floating-label">
-          <input placeholder="Username" type="text" name="username" id="username" autocomplete="off" />
-          <label for="username">Username:</label>
+          <input placeholder="Username" type="text" name="username" id="username" autoComplete="off" onChange={(e)=>{setUsername(e.target.value)}} />
+          <label htmlFor="username">Username</label>
           <div className="icon">
             <Image src="/username.png" alt="" width="30" height="30" className="image" />
           </div>
           {/* <a href="https://www.flaticon.com/free-icons/business-card" title="business card icons">Business card icons created by Tempo_doloe - Flaticon</a> */}
         </div>
         <div className="floating-label">
-          <input placeholder="Password" type="password" name="password" id="password" autocomplete="off" />
-          <label for="password">Password:</label>
+          <input placeholder="Password" type="password" name="password" id="password" autoComplete="off" onChange={(e)=>{setPassword(e.target.value)}} />
+          <label htmlFor="password">Password</label>
           <div className="icon">
             <Image src="/password.png" alt="" width="30" height="30" className="image" />
             {/* <a href="https://www.flaticon.com/free-icons/key" title="key icons">Key icons created by Tempo_doloe - Flaticon</a> */}
 
           </div>
         </div>
-        <button type="submit" onClick="return false;">Log in</button>
-      </form>
+        <button type="none" onClick={()=>{console.log(username, password);}}>Log in</button>
+      </div>
     </div>
-    )
-  }
+  )
+}
