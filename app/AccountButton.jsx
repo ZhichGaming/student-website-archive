bc035fc39d0af16006ba96ced53a24a84032e298
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2023-01-11 14:38:09 -0500

    made connect button redirect to login (fixed)

--- app/AccountButton.jsx
+++ app/AccountButton.jsx
@@ -5,21 +5,11 @@ import { useRef, useState } from "react";
 
 
 export default function AccountButton() {
-<<<<<<< HEAD
   const loginButton = useRef(null)
   let [user, setUser] = useState("")
-=======
-  let [user, setUser] = useState({})
-  let router = useRouter()
->>>>>>> a8e61c2c527c00b3dd6d1ecca5e72746bea5ada2
 
   const redirectToLogin = async() => {
-<<<<<<< HEAD
     loginButton.current.click()
-=======
-    console.log("login");
-    router.push("/login")
->>>>>>> a8e61c2c527c00b3dd6d1ecca5e72746bea5ada2
   }
     
 
