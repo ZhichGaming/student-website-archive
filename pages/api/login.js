7138299a275e32eec3d8391bbffac288bd994051
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2023-01-21 13:07:35 -0500

    still trying to add form to proxy

--- pages/api/login.js
+++ pages/api/login.js
@@ -11,7 +11,7 @@ export const config = {
 
 async function _handler(req, res) {
   return new Promise((resolve, reject) => {
-    proxy.on("proxyRes", (proxyRes, _request, _response) => {
+    proxy.on("proxyRes", (proxyRes, _req, res) => {
       console.log("test");
       var body = [];
       proxyRes.on("data", (chunk) => {
@@ -25,7 +25,7 @@ async function _handler(req, res) {
           res.status(200).json({link: re})
           resolve()
         }
-        res.status(403).send("Authentification failed");
+        res.end("Authentification failed");
         resolve()
       });
     });
@@ -33,15 +33,7 @@ async function _handler(req, res) {
       res.status().send(`${err}`)
       resolve()
     })
-    // let form = new FormData();
-    // form.append("_Fields", "GKfUxiJY9eL6Lgw19Ck6FDm7Y0/YzI2NRYHLQ86DaVe5c1HphAvntNdDDuRQSRQEzp+SHc/RWHhLdRHrH40G2zWtZh3USNSCejNryUeDmmgaKTbZnJspNyWQGfpB8cV+Ed085A")
-    // form.append("_NoSeqWeb", "GKfUxvmW7RTHuT0EqOUjjRiS0iOvZ2NAoTgdHW+VaWhOOstziX8jYd8UWCPHgyJtT0l1s/xqR9VFRzpTkrxgP28NMPI")
-    // form.append("btnConnecter.x", 0)
-    // form.append("btnConnecter.y", 0)
-    // for (let x in req.body) {
-    //   form.append(x, req.body[x])
-    // }
-    // req.body = form
+    
     proxy.web(req, res, {
       target: "https://portailc.jdlm.qc.ca/pednet/login.asp",
       changeOrigin: true,
@@ -51,6 +43,18 @@ async function _handler(req, res) {
 }
 
 export default function handler(req, res) {
+  console.log(req.body);
+
+  // let form = new FormData();
+  // form.append("_Fields", "GKfUxiJY9eL6Lgw19Ck6FDm7Y0/YzI2NRYHLQ86DaVe5c1HphAvntNdDDuRQSRQEzp+SHc/RWHhLdRHrH40G2zWtZh3USNSCejNryUeDmmgaKTbZnJspNyWQGfpB8cV+Ed085A")
+  // form.append("_NoSeqWeb", "GKfUxvmW7RTHuT0EqOUjjRiS0iOvZ2NAoTgdHW+VaWhOOstziX8jYd8UWCPHgyJtT0l1s/xqR9VFRzpTkrxgP28NMPI")
+  // form.append("btnConnecter.x", 0)
+  // form.append("btnConnecter.y", 0)
+  // for (let x in req.body) {
+  //   form.append(x, req.body[x])
+  // }
+  // req.body = form
+
   proxy.on('proxyRes', (proxyRes, req, res) => {
     var body = [];
     proxyRes.on('data', (chunk) => {
@@ -58,9 +62,18 @@ export default function handler(req, res) {
     });
     proxyRes.on('end', () => {
       body = Buffer.concat(body).toString();
-      res.end("my response to cli");
+      let re = body.match("Object moved")
+      if (re && re.length > 0) {
+        re = body.match(/"(.*?)"/g)[0].match(/[^\\"]+/)[0]
+        res.status(200).json({link: re})
+      }
+      res.status(403).send("Authentification failed");
     });
+    proxyRes.on("error", (err) => {
+      res.end(`${err}`)
+    })
   });
+
   proxy.web(req, res, {
     target: "https://portailc.jdlm.qc.ca/pednet/login.asp",
     selfHandleResponse : true,
