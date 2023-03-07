5d20a0f2a3d844a6004adcde548f3dffb369e3b9
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2023-03-07 09:12:26 -0500

    started backend with express

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 const app = express()
 
-app.post("/", () => {
-  
+const filter = require("./filter.js")
+
+// const port = process.env.PORT
+const port = "3001"
+
+app.use(bodyParser.json())
+app.use(filter)
+
+app.post("/login", (req, res) => {
+  console.log(req.body)
 })
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
+
