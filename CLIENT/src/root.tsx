21ac2508c6149d8facc3cf31a3a45a1135044526
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2023-05-12 10:51:49 -0400

    don't like qwikjs, switching to solidjs (not a good thing to do honestly)

--- CLIENT/src/root.tsx
+++ CLIENT/src/root.tsx
@@ -1,28 +1,53 @@
-import { component$ } from "@builder.io/qwik";
-import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from "@builder.io/qwik-city";
-import { RouterHead } from "./components/router-head/router-head";
+// @refresh reload
+import { Suspense } from "solid-js";
+import {
+  useLocation,
+  A,
+  Body,
+  ErrorBoundary,
+  FileRoutes,
+  Head,
+  Html,
+  Meta,
+  Routes,
+  Scripts,
+  Title,
+} from "solid-start";
+import "./root.css";
 
-import "./global.css";
-
-export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Dont remove the `<head>` and `<body>` elements.
-   */
-
+export default function Root() {
+  const location = useLocation();
+  const active = (path: string) =>
+    path == location.pathname
+      ? "border-sky-600"
+      : "border-transparent hover:border-sky-600";
   return (
-    <QwikCityProvider>
-      <head>
-        <meta charSet="utf-8" />
-        <RouterHead />
-      </head>
-      <body lang="en">
-        <RouterOutlet />
-        <ServiceWorkerRegister />
-      </body>
-    </QwikCityProvider>
+    <Html lang="en">
+      <Head>
+        <Title>SolidStart - With TailwindCSS</Title>
+        <Meta charset="utf-8" />
+        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Body>
+        <Suspense>
+          <ErrorBoundary>
+            <nav class="bg-sky-800">
+              <ul class="container flex items-center p-3 text-gray-200">
+                <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
+                  <A href="/">Home</A>
+                </li>
+                <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
+                  <A href="/about">About</A>
+                </li>
+              </ul>
+            </nav>
+            <Routes>
+              <FileRoutes />
+            </Routes>
+          </ErrorBoundary>
+        </Suspense>
+        <Scripts />
+      </Body>
+    </Html>
   );
-});
-
+}
