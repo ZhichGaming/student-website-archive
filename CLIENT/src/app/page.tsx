d4ee328afa40c1163bce496d2b68f7853d567ed2
ZhichGaming <nick.zhicheng@gmail.com>
2023-06-06 19:53:49 -0400

    Refactor: Adjust to Tailwind, change <a> to <Link>
    ez (not ez)

--- CLIENT/src/app/page.tsx
+++ CLIENT/src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -8,9 +9,9 @@ export default function Home() {
 
 function Toolbar() {
   return (
-    <div className="toolbar">
+    <div className="toolbar w-full h-16 px-5 fixed top-0 z-50 flex items-center justify-center">
       {/* Bro I tried everything I can't get local images to work in assets folder... */}
-      <img src="./favicon.ico" className="toolbar-logo"/>
+      <img src="./favicon.ico" className="toolbar-logo mr-auto h-8"/>
 
       <div className="toolbar-buttons-group">
         <ToolbarLink url="" label="Home" />
@@ -18,9 +19,9 @@ function Toolbar() {
         <ToolbarLink url="/portal" label="Portal" />
       </div>
 
-      <div className="toolbar-profile-group">
+      <div className="toolbar-profile-group flex items-center ml-auto">
         <p>Your Name</p>
-        <img src="https://64.media.tumblr.com/ef4fdf8138e198106cc18fcc5300c1cb/d0bb637b9240f4c1-6f/s1280x1920/2e5dc11ff1e3b7955214477f422fdaec409c9cbc.jpg" alt="Profile picture" className="toolbar-pfp"/>
+        <img src="https://64.media.tumblr.com/ef4fdf8138e198106cc18fcc5300c1cb/d0bb637b9240f4c1-6f/s1280x1920/2e5dc11ff1e3b7955214477f422fdaec409c9cbc.jpg" alt="Profile picture" className="toolbar-pfp w-12 h-12 object-cover rounded-full ml-2"/>
       </div>
     </div>
   );
@@ -39,9 +40,12 @@ function ToolbarLink({ url, label }: { url: string; label: string }) {
     }
   }, [url]);
 
+  const toolbarButtonItemCss = "toolbar-button-item inline-block text-center ml-4 mr-4 opacity-50 hover:opacity-100 hover:duration-700"
+  const toolbarButtonItemAnimationCss = "after:block after:origin-center after:scale-x-0 after:border-b-2 after:transition-all after:duration-500 after:ease-in-out hover:after:scale-x-100 hover:after:border-[#5F9EA0]"
+
   return (
-    <a href={url != "" ? url : "../"} className={`toolbar-button-item ${isActive ? "current-page-button" : ""}`}>
+    <Link href={url != "" ? url : "../"} className={`toolbar-button-item ${toolbarButtonItemCss} ${isActive ? "current-page-button border-[#5F9EA0] opacity-100 border-b-4" : toolbarButtonItemAnimationCss}`}>
       {label}
-    </a>
+    </Link>
   );
 }
