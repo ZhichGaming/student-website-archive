e5c00602c1811dea63369c8143b61ecf80bcb963
Ch3ru09 <xliang0905@gmail.com>
2023-06-13 00:15:29 -0400

    added login api

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,5 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export default function POST(request: NextRequest) {
-  
+export function GET(request: NextRequest) {
+  return NextResponse.json({ get: "got" });
+}
+
+export async function POST(request: NextRequest) {
+  const b = await request.json();
+  const body = { grant_type: "password", ...b };
+
+  const res = await fetch("https://portailc.jdlm.qc.ca/pednet/mobile/token", {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams(Object.entries(body)).toString(),
+  });
+  const data = await res.json();
+  return NextResponse.json(data);
 }
