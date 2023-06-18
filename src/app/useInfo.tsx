0713c92f4d47dff9b61469bf21a084ec8ac40030
Ch3ru09 <xliang0905@gmail.com>
2023-06-18 14:24:29 -0400

    still doing hook for grades

--- src/app/useInfo.tsx
+++ src/app/useInfo.tsx
@@ -2,7 +2,7 @@
 
 import { createContext, useContext, useEffect, useState } from "react";
 import type { ReactNode } from "react";
-import type { Info, Today, Classes } from "./types";
+import type { Info, Today, Classes, Grades } from "./types";
 
 const infoContext = createContext<InfoContext>(null);
 
@@ -11,6 +11,7 @@ function InfoContextProvider({ children }: { children: ReactNode }) {
   const [info, setInfo] = useState<Info>();
   const [today, setToday] = useState<Today>();
   const [classes, setClasses] = useState<Classes>();
+  const [grades, setGrades] = useState<Grades>();
 
   useEffect(() => {
     localStorage.setItem("token", token);
@@ -40,30 +41,41 @@ function InfoContextProvider({ children }: { children: ReactNode }) {
 
   const getGrade = async (tokenData: string, classes: Classes) => {
     // TODO: return data in object
+    const allGrades = [];
 
     let semesters = 3;
-    for (let j = 1; j <= semesters; j++) {
-      classes.forEach(async (x) => {
-        for (let i = 0; i < parseInt(x.nbCompetencies); i++) {
-          const params = new URLSearchParams();
-          params.append("Competence", x.id + "~" + i);
-          params.append("Etape", j.toString());
-          params.append("cleClasse", x.id);
+    classes.forEach(async (x) => {
+      const tempClass = [];
+      setTimeout(async () => {
+        for (let j = 1; j <= semesters; j++) {
+          const semesterGrades = [];
+          for (let i = 0; i < parseInt(x.nbCompetencies); i++) {
+            const params = new URLSearchParams();
+            params.append("Competence", x.id + "~" + i);
+            params.append("Etape", j.toString());
+            params.append("cleClasse", x.id);
 
-          const res = await fetch("/api/grades?" + params, {
-            method: "GET",
-            mode: "cors",
-            next: { revalidate: 60 },
-            cache: "default",
-            headers: {
-              authorization: "Bearer " + tokenData,
-            },
-          });
-          const data = await res.json();
-          console.log(data);
+            const res = await fetch("/api/grades?" + params, {
+              method: "GET",
+              mode: "cors",
+              next: { revalidate: 60 },
+              cache: "default",
+              headers: {
+                authorization: "Bearer " + tokenData,
+              },
+            });
+            const data = await res.json();
+            console.log(data);
+          }
         }
-      });
-    }
+      }, 1000);
+
+      const out = {};
+      out[x.name] = tempClass;
+      allGrades.push(out);
+    });
+
+    setGrades(allGrades);
   };
 
   const getClasses = async (tokenData: string, id: string, time: string) => {
