06c64f785c9f10e06f6ea4c8299ce606d4d41866
ZhichGaming <nick.zhicheng@gmail.com>
2023-06-23 19:07:59 -0400

    Feat: Grades UI

---
+++ src/app/(home)/grades/_components/GradesCard.tsx
@@ -0,0 +1,41 @@
+export default function GradesCard({ gradeCategories, selectedClass, term, search }: { gradeCategories: any[], selectedClass: string, term: string, search: string }) {
+    var gradeCategoriesHtml = gradeCategories.filter((gradeCategory) => (
+        (gradeCategory.category.toLowerCase().includes(search.toLowerCase()) || gradeCategory.class.toLowerCase().includes(search.toLowerCase())) && 
+        (gradeCategory.class == selectedClass || selectedClass == "All")
+    )).map((gradeCategory) => (
+        <div className="flex flex-col bg-black bg-opacity-10 rounded-md p-4 space-y-2">
+            <div className="flex justify-between mb-2">
+                <p className="text-lg">{gradeCategory.class}</p>
+                <p className="text-lg opacity-50">{gradeCategory.category} ({gradeCategory.weight}%)</p>
+            </div>
+
+            <div className="bg-white rounded-md p-2 space-y-2">
+                <div className="flex text-sm">
+                    <p className="opacity-50 flex-grow">Title</p>
+                    <p className="opacity-50 w-56">Category</p>
+                    <p className="opacity-50 w-20">Weight</p>
+                    <p className="opacity-50 w-32">Average</p>
+                    <p className="font-semibold w-32">Result</p>
+                </div>
+
+                <hr/>
+
+                {gradeCategory.grades.map((grade) => (
+                    <div className="flex justify-between text-sm">
+                        <p className="flex-grow">{grade.name}</p>
+                        <p className="w-56">{grade.category}</p>
+                        <p className="w-20">{grade.weight}</p>
+                        <p className="w-32">{grade.average}/{grade.total} ({(grade.average/grade.total*100).toFixed()}%)</p>
+                        <p className="w-32">{grade.value}/{grade.total} ({(grade.value/grade.total*100).toFixed()}%)</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    ));
+
+    return (
+        <div className="flex flex-col bg-white p-4 rounded-md h-full space-y-6">
+            {gradeCategoriesHtml}
+        </div>
+    );
+}
---
+++ src/app/(home)/grades/_components/NavigationCard.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { useState } from "react";
+
+export default function NavigationCard({ gradeCategories, selectedClass, onSelectedClassChange, term, onTermChange, search, onSearchChange }: { gradeCategories: any[], selectedClass: string, onSelectedClassChange: (selectedClass: string) => void, term: string, onTermChange: (term: string) => void, search: string, onSearchChange: (search: string) => void }) {
+    const classOptionsSet = [new Set(gradeCategories.map((gradeCategory) => gradeCategory.class))];
+    const classOptions = Array.from(classOptionsSet[0]);
+
+    const handleSelectedClassChange = (event) => {
+        onSelectedClassChange(event.target.value);
+    }
+
+    const handleTermChange = (event) => {
+        onTermChange(event.target.value);
+    }
+
+    const handleSearchChange = (event) => {
+        onSearchChange(event.target.value);
+    }
+
+    return (
+        <div className="flex bg-white items-center space-x-2 p-2 px-6 rounded-md justify-between">
+            <div className="flex space-x-2">
+                <select className="border border-gray-300 rounded-md p-1 w-64" onChange={handleSelectedClassChange}>
+                    <option>All</option>
+                    {classOptions.map((option) => (
+                        <option>{option}</option>
+                    ))}
+                </select>
+                <select className="border border-gray-300 rounded-md p-1 w-24" onChange={handleTermChange}>
+                    <option>Term 1</option>
+                    <option>Term 2</option>
+                    <option>Term 3</option>
+                </select>
+            </div>
+
+            <input type="text" placeholder="Search" className="border border-gray-300 rounded-md p-1 w-64 px-2" onChange={handleSearchChange}/>
+        </div>
+    );
+}
