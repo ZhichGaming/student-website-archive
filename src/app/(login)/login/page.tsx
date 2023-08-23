2bba0d58ac27b9adebd27cc5e81806e41f302bdf
ZhichGaming <nick.zhicheng@gmail.com>
2023-08-23 18:48:23 -0400

    Feat: More login page styling

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -3,8 +3,10 @@ import LoginForm from "./LoginForm";
 export default function LoginPage() {
   return (
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-      <div className="absolute w-full h-full bg-entrance bg-cover bg-bottom blur-md"></div>
-      <div className="relative py-3 sm:max-w-2xl sm:mx-auto">
+      <div className="absolute w-full h-full -translate-x-[800px] clip-diagonal">
+        <div className="absolute w-full h-full bg-entrance bg-cover bg-left-bottom translate-x-80"></div>
+      </div>
+      <div className="relative py-3 sm:max-w-2xl sm:mx-auto translate-x-72">
         {/* Blue background */}
         <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
